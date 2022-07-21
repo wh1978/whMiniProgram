14b666944c4e9d776945f9d901f8140c2bae4318
wh1978 <771776719@qq.com>
2022-07-21 15:19:35 +0800

    first commit mini

---
+++ service/index.js
@@ -0,0 +1,51 @@
+import { TOKEN_KEY } from '../constants/token-const'
+
+const token = wx.getStorageSync(TOKEN_KEY)
+
+const BASE_URL = "http://123.207.32.32:9001"
+// 用我已经部署好的
+const LOGIN_BASE_URL = "http://123.207.32.32:3000"
+// 用我给你的登录服务器代码,自己部署
+// const LOGIN_BASE_URL = "http://localhost:3000"
+
+class HYRequest {
+  constructor(baseURL, authHeader = {}) {
+    this.baseURL = baseURL
+    this.authHeader = authHeader
+  }
+
+  request(url, method, params, isAuth = false, header = {}) {
+    const finalHeader = isAuth ? { ...this.authHeader, ...header }: header
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: this.baseURL + url,
+        method: method,
+        header: finalHeader,
+        data: params,
+        success: function(res) {
+          resolve(res.data)
+        },
+        fail: reject
+      })
+    })
+  }
+
+  get(url, params, isAuth = false, header) {
+    return this.request(url, "GET", params, isAuth, header)
+  }
+
+  post(url, data, isAuth = false, header) {
+    return this.request(url, "POST", data, isAuth, header)
+  }
+}
+
+const hyRequest = new HYRequest(BASE_URL)
+
+const hyLoginRequest = new HYRequest(LOGIN_BASE_URL, {
+  token
+})
+
+export default hyRequest
+export {
+  hyLoginRequest
+}
